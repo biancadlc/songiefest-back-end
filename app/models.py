1b359c84a14b8d67da016888176674d463d67fbf
Thao Le <thaole0325@gmail.com>
2023-01-26 11:47:15 -0500

    finalized Profile model, made changes to other models

--- app/models.py
+++ app/models.py
@@ -5,9 +5,6 @@ from django.conf import settings
 from autoslug import AutoSlugField # makes meaningful URLS users/thao
 
 # always have created at & updated at attributes (on every model)
-# HOW ARE WE MAKING THE POST THAT GENERATES MUSIC STATS? DOES USER POST THAT?
-# MUSIC STATS = POST? 
-# MODEL FOR ACCOUNT & PROFILE?
 
 # changed name to AppUser to avoid confusion w/ Django's default User model
 # project's settings.py --> need to tell Django use custom model instead of default 
@@ -39,7 +36,9 @@ class AppUser(models.Model):
     
     
 # The related_name attribute in the ForeignKey fields is extremely useful. 
-# It let’s us define a meaningful name for the reverse relationship.
+# defines a meaningful name for the reverse relationship.
+# one instance of the model with the foreign key can be related to 
+# multiple instances of the model it references
     
 
 # ==== MusicStats model or Profile model?? === # 
@@ -53,19 +52,21 @@ class MusicPost(models.Model):   # post that shows music stats
     
 
 class MusicStat(models.Model):    # actual music stats
-    title = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
-    music_post = models.ForeignKey(MusicPost, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    artist = models.CharField(max_length=30)
+    album = models.CharField(max_length=30)
+    music_post = models.ForeignKey(MusicPost, on_delete=models.CASCADE, related_name='music_stats')
     play_count = models.IntegerField(null=True)
     date_modified = models.DateTimeField(auto_now=True)
     date_published = models.DateTimeField(auto_now_add=True)
     
+    # playcount null allow bc this might not be known when model is first created
+    
     
 # ===== COMMENT model  links comment w/ the post(music stat) & the user ===== #
 class Comment(models.Model):
-    music_post= models.ForeignKey(MusicPost, related_name='comment', on_delete=models.CASCADE)
-    user = models.ForeignKey(AppUser, related_name='comment', on_delete=models.CASCADE)
+    music_post= models.ForeignKey(MusicPost, related_name='comments', on_delete=models.CASCADE)
+    app_user = models.ForeignKey(AppUser, related_name='comments', on_delete=models.CASCADE)
     comment = models.TextField(max_length=500)
     date_published = models.DateTimeField(auto_now_add=True)
     
@@ -73,35 +74,36 @@ class Comment(models.Model):
 # ====== LIKES model   stores like info   ====== #
 class Like(models.Model):
     # user = represents user who liked post, deleting user deletes like
-    user = models.ForeignKey(AppUser, related_name='like', on_delete=models.CASCADE)
-    music_post = models.ForeignKey(MusicPost, related_name='like', on_delete=models.CASCADE)
+    user = models.ForeignKey(AppUser, related_name='likes', on_delete=models.CASCADE)
+    music_post = models.ForeignKey(MusicPost, related_name='likes', on_delete=models.CASCADE)
     # post = the post on which the like is given, deleting post deletes all likes 
 
     # use same ForeignKey, to differentiate need
     
     
 # ==== Profile model ===== # uses Django User model
+# 1:1 relationship w/ AppUser, each profile is unique to a user
+# each user can have only one profile.
+# if User deleted, profile destroyed too
+# ForeignKey is used for one-to-many relationships
+# need to use OntToOneField for one-to-one
+# related_name singular for OneToOneField
 
 class Profile(models.Model): 
-    # 1:1 relationship w/ Django User model
-    # if User deleted, profile destroyed too
-    user = models.OneToOneField(AppUser, on_delete=models.CASCADE) 
-    # optional? for storing profile pic of user
-    image = models.ImageField(default='default.png', upload_to='profile_pics') 
-    # use AutoSlugField, set it to make a slug from user field
-    slug = AutoSlugField(populate_from='user')
-    # store small intro? blank=True means it can be left blank
-    bio = models.CharField(max_length=255, blank=True)
-    # many to many w/ Profile model, can be left blank 
-    # every user can have multiple friends & can be frends w/ multiple ppl
-    # friends = models.ManyToManyField("Profile", blank=True)x     # stretch
+    app_user = models.OneToOneField(AppUser, on_delete=models.CASCADE, related_name='profile') 
+    image = models.ImageField(default='default.png', upload_to='profile_pics', blank=True) 
+    # use AutoSlugField, set it to make a slug from username field
+    slug = AutoSlugField(populate_from='app_user.username', unique=True)
+    # store small bio? blank=True means it can be left blank
+    bio = models.CharField(max_length=255, blank=True)    
     
-    
-    
-            
-    # define get_absolute_url toget the abs URL for that profile
+
+    # returns URL for profle page of user by using `username` attribute 
+    # of `app_user` field 
     def get_absolute_url(self):
-        return "/users/{}".format(self.slug)      
+        return "/app_users/{}".format(self.app_user.username)      
 
-
+    # specifies that the URL should include "app_users" followed by 
+    # value of the variable "slug" which is coming from Profile model
+    # it is being populated w/ value of the app_user's username using the AutoSlugField.
     
